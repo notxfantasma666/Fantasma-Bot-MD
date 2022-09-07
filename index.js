a02bf47b4ccf2febc6932d80bbc6130691e6d944
Fantasma zZ <111621189+notxfantasma666@users.noreply.github.com>
2022-09-07 17:58:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ say('Fantasma - Bot\nWhatsApp Bot MD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Bot creado por Bruno Sobrino`, {
+say(`Bot creado por Fantasma zz`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
